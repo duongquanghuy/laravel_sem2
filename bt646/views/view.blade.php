2e18de12541b1d69aa61c156e21fe666976df391
duong qaung huy <huydqth1810012@fpt.edu.vn>
2019-09-29 20:12:39 +0700

    bt646

---
+++ bt646/views/view.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>bt646 view</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Bootstrap Table with Add and Delete Row Feature</title>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round|Open+Sans">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="panel panel-primary" >
+			<div class="panel-heading">
+				danh sach phong			
+			</div>
+			<div class="panel-body">
+				<table class="table table-bordered">
+
+					<thead>
+						<tr>
+							<th>stt</th>
+							<th>id</th>
+							<th>ma phong</th>
+							<th>loai phong</th>
+							<th>tang</th>
+							<th>gia tien</th>
+						</tr>
+					</thead>
+					<tbody>
+						@foreach($hotelList as $items)
+							<tr>
+								<td>{{ $index++ }}</td>
+								<td>{{ $items->id }} </td>
+								<td>{{ $items->maPhong }} </td>
+								<td>{{ $items->loaiPhong}} </td>
+								<td>{{ $items->tang }} </td>
+								<td>{{ $items->giaTien }} </td>
+
+							</tr>
+
+						@endforeach
+								
+					</tbody>
+				</table>
+				{{ $hotelList->links() }}
+			</div>
+			
+		</div>
+	</div>
+
+</body>
+</html>
